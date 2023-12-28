b7025c9ab88d173fdcfcce9e5f9e2347e1e70fc9
masud-ran <naushadlaimasud999@gmail.com>
2023-12-28 05:32:35 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import store from "./store";
+import App from "./App";
+import {BrowserRouter} from "react-router-dom"; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const theme = createTheme();
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE,
+};
+
+ReactDOM.render(
+  <>
+    {/* Wrap the entire application with BrowserRouter */}
+    <BrowserRouter>
+    
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <AlertProvider template={AlertTemplate} {...options}>
+          <App />
+        </AlertProvider>
+      </Provider>
+    </ThemeProvider>
+    </BrowserRouter>
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
